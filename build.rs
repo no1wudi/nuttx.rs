1225b824a5779b97989facb97f571a09fb52235d
Huang Qi <huangqi3@xiaomi.com>
2020-07-03 16:14:33 +0800

    build: Choose link dir according to build target

--- build.rs
+++ build.rs
@@ -3,32 +3,42 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    let out_dir = env::var_os("NUTTX_DIR");
+    let target = env::var("TARGET").unwrap();
+    if !target.starts_with("x86") {
+        let nuttx_dir = env::var_os("NUTTX_DIR");
+        if let Some(nuttx_dir) = nuttx_dir {
+            let nuttx = Path::new(&nuttx_dir);
 
-    if let Some(out_dir) = out_dir {
-        let target = Path::new(&out_dir);
-        Command::new("make").current_dir(&out_dir).status().unwrap();
-        println!("cargo:rustc-link-search={}/staging", target.display());
-        println!(
-            "cargo:rustc-link-search={}/arch/arm/src/board",
-            target.display()
-        );
-        println!(
-            "cargo:rustc-link-search={}/arch/risc-v/src/board",
-            target.display()
-        );
-        println!("cargo:rustc-link-lib=static=sched");
-        println!("cargo:rustc-link-lib=static=drivers");
-        println!("cargo:rustc-link-lib=static=boards");
-        println!("cargo:rustc-link-lib=static=arch");
-        println!("cargo:rustc-link-lib=static=c");
-        println!("cargo:rustc-link-lib=static=mm");
-        println!("cargo:rustc-link-lib=static=xx");
-        println!("cargo:rustc-link-lib=static=apps");
-        println!("cargo:rustc-link-lib=static=fs");
-        println!("cargo:rustc-link-lib=static=binfmt");
-        println!("cargo:rustc-link-lib=static=board");
-    } else {
-        println!("cargo:warning=No NuttX source dir found!");
+            if target.starts_with("thumb") {
+                println!(
+                    "cargo:rustc-link-search={}/arch/arm/src/board",
+                    nuttx.display()
+                );
+            } else if target.starts_with("riscv") {
+                println!(
+                    "cargo:rustc-link-search={}/arch/risc-v/src/board",
+                    nuttx.display()
+                );
+            } else {
+                println!("cargo:warning=Platform not support:{}", target);
+            }
+
+            Command::new("make").current_dir(&nuttx).status().unwrap();
+
+            println!("cargo:rustc-link-search={}/staging", nuttx.display());
+            println!("cargo:rustc-link-lib=static=sched");
+            println!("cargo:rustc-link-lib=static=drivers");
+            println!("cargo:rustc-link-lib=static=boards");
+            println!("cargo:rustc-link-lib=static=arch");
+            println!("cargo:rustc-link-lib=static=c");
+            println!("cargo:rustc-link-lib=static=mm");
+            println!("cargo:rustc-link-lib=static=xx");
+            println!("cargo:rustc-link-lib=static=apps");
+            println!("cargo:rustc-link-lib=static=fs");
+            println!("cargo:rustc-link-lib=static=binfmt");
+            println!("cargo:rustc-link-lib=static=board");
+        } else {
+            println!("cargo:warning=No NuttX source dir found!");
+        }
     }
 }
